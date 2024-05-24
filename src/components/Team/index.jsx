b6d13d2940d154bbf9ef1b93a5d3fc345676dffe
joserailldo <joserailldo@gmail.com>
2024-05-23 21:14:08 -0300

    refeactor componentes

--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -1,10 +1,21 @@
 import "./styles.css"
+
 import { Team } from "./src/pattern/composition"
+
+const mock = [
+    1, 2, 3, 4
+]
+
 
 export default function TeamComposition() {
     return (
         <div className="team-container">
             <Team.Section />
+
+            {mock.map((element) => {
+
+                return <Team.Content key={element} />
+            })}
             <Team.Content />
             <Team.Main />
         </div>
--- src/components/Team/src/pattern/composition/team-content.composition.jsx
+++ src/components/Team/src/pattern/composition/team-content.composition.jsx
@@ -14,37 +14,7 @@ export default function Content() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
             </p>
 
-            <div className="content-title">
-                <h1>Question text goes here</h1>
-               
-            </div>
-            <p className='content-description'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
-            </p>
-
-            <div className="content-title">
-                <h1>Question text goes here</h1>
-               
-            </div>
-            <p className='content-description'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
-            </p>
-
-            <div className="content-title">
-                <h1>Question text goes here</h1>
-               
-            </div>
-            <p className='content-description'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
-            </p>
-
-            <div className="content-title">
-                <h1>Question text goes here</h1>
-               
-            </div>
-            <p className='content-description'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
-            </p>
+            
             
             <div className="content-icons">
                    
